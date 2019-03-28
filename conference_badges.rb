d5c079d8d96c614d7a92c509efdc17fcb8c78f0f
Ashley Miller <asm2fe@virginia.edu>
2019-03-28 00:34:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,7 @@ end
 def batch_badge_creator(array)
   array.collect! {|x| badge_maker(x)}
 end
+
+def assign_rooms()
+  array.each_with_index! {|x, i| "Hello, #{x}! You'll be assigned to room #{i+1}! "}
+end
